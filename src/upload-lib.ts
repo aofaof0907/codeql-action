c40a4e2968c62c3f58b51e133062dd0042e89d5e
Robert <robertbrignull@github.com>
2021-01-04 13:12:30 +0000

    Add test of finding SARIF files recursively

--- src/upload-lib.ts
+++ src/upload-lib.ts
@@ -81,25 +81,58 @@ export interface UploadStatusReport {
   num_results_in_sarif?: number;
 }
 
+// Recursively walks a directory and returns all SARIF files it finds.
+// Does not follow symlinks.
+export function findSarifFilesInDir(sarifPath: string): string[] {
+  const sarifFiles: string[] = [];
+  const walkSarifFiles = (dir: string) => {
+    const entries = fs.readdirSync(dir, { withFileTypes: true });
+    for (const entry of entries) {
+      if (entry.isFile() && entry.name.endsWith(".sarif")) {
+        sarifFiles.push(path.resolve(dir, entry.name));
+      } else if (entry.isDirectory()) {
+        walkSarifFiles(path.resolve(dir, entry.name));
+      }
+    }
+  };
+  walkSarifFiles(sarifPath);
+  return sarifFiles;
+}
+
 // Uploads a single sarif file or a directory of sarif files
 // depending on what the path happens to refer to.
 // Returns true iff the upload occurred and succeeded
-export async function uploadFromActions(
+export async function upload(
   sarifPath: string,
   repositoryNwo: RepositoryNwo,
   commitOid: string,
   ref: string,
-  analysisKey: string,
-  analysisName: string,
-  workflowRunID: number,
+  analysisKey: string | undefined,
+  analysisName: string | undefined,
+  workflowRunID: number | undefined,
   checkoutPath: string,
-  environment: string,
+  environment: string | undefined,
   gitHubVersion: util.GitHubVersion,
   apiDetails: api.GitHubApiDetails,
+  mode: util.Mode,
   logger: Logger
 ): Promise<UploadStatusReport> {
+  if (!fs.existsSync(sarifPath)) {
+    throw new Error(`Path does not exist: ${sarifPath}`);
+  }
+
+  let sarifFiles: string[];
+  if (fs.lstatSync(sarifPath).isDirectory()) {
+    sarifFiles = findSarifFilesInDir(sarifPath);
+    if (sarifFiles.length === 0) {
+      throw new Error(`No SARIF files found to upload in "${sarifPath}".`);
+    }
+  } else {
+    sarifFiles = [sarifPath];
+  }
+
   return await uploadFiles(
-    getSarifFilePaths(sarifPath),
+    sarifFiles,
     repositoryNwo,
     commitOid,
     ref,
@@ -110,65 +143,9 @@ export async function uploadFromActions(
     environment,
     gitHubVersion,
     apiDetails,
-    "actions",
+    mode,
     logger
   );
-}
-
-// Uploads a single sarif file or a directory of sarif files
-// depending on what the path happens to refer to.
-// Returns true iff the upload occurred and succeeded
-export async function uploadFromRunner(
-  sarifPath: string,
-  repositoryNwo: RepositoryNwo,
-  commitOid: string,
-  ref: string,
-  checkoutPath: string,
-  gitHubVersion: util.GitHubVersion,
-  apiDetails: api.GitHubApiDetails,
-  logger: Logger
-): Promise<UploadStatusReport> {
-  return await uploadFiles(
-    getSarifFilePaths(sarifPath),
-    repositoryNwo,
-    commitOid,
-    ref,
-    undefined,
-    undefined,
-    undefined,
-    checkoutPath,
-    undefined,
-    gitHubVersion,
-    apiDetails,
-    "runner",
-    logger
-  );
-}
-
-function getSarifFilePaths(sarifPath: string) {
-  const sarifFiles: string[] = [];
-  if (!fs.existsSync(sarifPath)) {
-    throw new Error(`Path does not exist: ${sarifPath}`);
-  }
-  if (fs.lstatSync(sarifPath).isDirectory()) {
-    const walkSarifFiles = (dir: string) => {
-      const entries = fs.readdirSync(dir, { withFileTypes: true });
-      for (const entry of entries) {
-        if (entry.isFile() && entry.name.endsWith(".sarif")) {
-          sarifFiles.push(path.resolve(dir, entry.name));
-        } else if (entry.isDirectory()) {
-          walkSarifFiles(path.resolve(dir, entry.name));
-        }
-      }
-    };
-    walkSarifFiles(sarifPath);
-    if (sarifFiles.length === 0) {
-      throw new Error(`No SARIF files found to upload in "${sarifPath}".`);
-    }
-  } else {
-    sarifFiles.push(sarifPath);
-  }
-  return sarifFiles;
 }
 
 // Counts the number of results in the given SARIF file
